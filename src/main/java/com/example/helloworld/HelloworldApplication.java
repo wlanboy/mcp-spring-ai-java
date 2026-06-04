57e159de9eb76f409e0fa3a2daf8a9b8c0341aa4
wlanboy <wlanboy@github.com>
2026-06-04 10:02:57 +0200

    Application with ToolCallbackProvider

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@Bean
+	public ToolCallbackProvider helloWorldTools(HelloWorldTools tools) {
+		return MethodToolCallbackProvider.builder().toolObjects(tools).build();
+	}
+}
